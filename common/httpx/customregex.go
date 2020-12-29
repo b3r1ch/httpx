33c1f9f88a4fc2945df0e81d3e61dd0b59d5b643
b3r1ch <b3r1ch@protonmail.com>
2020-12-29 17:24:47 +0800

    只输出`-custom-regex`中的子表达式内容

--- common/httpx/customregex.go
+++ common/httpx/customregex.go
@@ -10,7 +10,7 @@ func ExtractInfoByCustomRegex(r *Response, regex string) (info string) {
 	var re = regexp.MustCompile(regex)
 	matchs := re.FindAllStringSubmatch(r.Raw, -1)
 	for _, match := range matchs {
-		info += strings.Join(match, "_")
+		info += strings.Join(match[1:], " | ")
 	}
 	return
 }
